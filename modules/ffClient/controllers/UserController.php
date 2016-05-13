14e8098db1590629ca859fb67c0211501c45711b
Cranky4 <cranky4.89@gmail.com>
2016-05-13 12:15:55 +0300

    close user pages

--- modules/ffClient/controllers/UserController.php
+++ modules/ffClient/controllers/UserController.php
@@ -37,46 +37,46 @@
         /**
          * @return string
          */
-        public function actionIndex()
-        {
-            $query = User::find();
-            $provider = new ActiveDataProvider([
-                'query' => $query,
-            ]);
-
-            return $this->render('index', [
-                'provider' => $provider,
-            ]);
-        }
+//        public function actionIndex()
+//        {
+//            $query = User::find();
+//            $provider = new ActiveDataProvider([
+//                'query' => $query,
+//            ]);
+//
+//            return $this->render('index', [
+//                'provider' => $provider,
+//            ]);
+//        }
 
         /**
          * @return string
          */
-        public function actionCreate()
-        {
-            $model = new User();
-
-            //get post data
-            if ($data = \Yii::$app->request->post('User')) {
-                $route = $this->getApiRoute('user_create');
-                //send data to ff api and check errors
-                $response = $this->doRequest($route, $data);
-                $model->setAttributes($data, false);
-                $model->checkApiErrors($response);
-                if (!$model->hasErrors()) {
-                    $model->ff_id = $response['id'];
-                    $model->api_key = $response['api_key'];
-                    $model->setPassword($data['password']);
-                    $model->generateAuthKey();
-                    $model->save(false);
-                    $this->redirect(['view', 'id' => $model->id]);
-                }
-            }
-
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
+//        public function actionCreate()
+//        {
+//            $model = new User();
+//
+//            //get post data
+//            if ($data = \Yii::$app->request->post('User')) {
+//                $route = $this->getApiRoute('user_create');
+//                //send data to ff api and check errors
+//                $response = $this->doRequest($route, $data);
+//                $model->setAttributes($data, false);
+//                $model->checkApiErrors($response);
+//                if (!$model->hasErrors()) {
+//                    $model->ff_id = $response['id'];
+//                    $model->api_key = $response['api_key'];
+//                    $model->setPassword($data['password']);
+//                    $model->generateAuthKey();
+//                    $model->save(false);
+//                    $this->redirect(['view', 'id' => $model->id]);
+//                }
+//            }
+//
+//            return $this->render('create', [
+//                'model' => $model,
+//            ]);
+//        }
 
         /**
          * @param $id
@@ -84,37 +84,37 @@
          * @return string
          * @throws \yii\web\NotFoundHttpException
          */
-        public function actionUpdate($id)
-        {
-            /**
-             * @var User $model
-             */
-            $model = User::findOne($id);
-            if (null === $model) {
-                throw new NotFoundHttpException("User not found!");
-            }
-
-            $model->scenario = \app\modules\ffClient\models\User::SCENARIO_UPDATE;
-
-            //get post data
-            if ($model->load(\Yii::$app->request->post())) {
-                $params = [
-                    'id' => $model->ff_id,
-                ];
-                $url = $this->getApiRoute('user_update', $params);
-                //send data to ff api and check errors
-                $response = $this->doRequest($url, $model->getAttributes(), 'PATCH');
-                $model->checkApiErrors($response);
-                if (!$model->hasErrors()) {
-                    $model->save(false);
-                    $this->redirect(['/ffClient/user/index']);
-                }
-            }
-
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
+//        public function actionUpdate($id)
+//        {
+//            /**
+//             * @var User $model
+//             */
+//            $model = User::findOne($id);
+//            if (null === $model) {
+//                throw new NotFoundHttpException("User not found!");
+//            }
+//
+//            $model->scenario = \app\modules\ffClient\models\User::SCENARIO_UPDATE;
+//
+//            //get post data
+//            if ($model->load(\Yii::$app->request->post())) {
+//                $params = [
+//                    'id' => $model->ff_id,
+//                ];
+//                $url = $this->getApiRoute('user_update', $params);
+//                //send data to ff api and check errors
+//                $response = $this->doRequest($url, $model->getAttributes(), 'PATCH');
+//                $model->checkApiErrors($response);
+//                if (!$model->hasErrors()) {
+//                    $model->save(false);
+//                    $this->redirect(['/ffClient/user/index']);
+//                }
+//            }
+//
+//            return $this->render('update', [
+//                'model' => $model,
+//            ]);
+//        }
 
         /**
          * @param $id
@@ -122,17 +122,17 @@
          * @return string
          * @throws \yii\web\HttpException
          */
-        public function actionView($id)
-        {
-            $model = User::findOne($id);
-            if (null === $model) {
-                throw new NotFoundHttpException("User not found!");
-            }
-
-            return $this->render('view', [
-                'model' => $model,
-            ]);
-        }
+//        public function actionView($id)
+//        {
+//            $model = User::findOne($id);
+//            if (null === $model) {
+//                throw new NotFoundHttpException("User not found!");
+//            }
+//
+//            return $this->render('view', [
+//                'model' => $model,
+//            ]);
+//        }
 
         /**
          * @param $id
@@ -140,13 +140,13 @@
          * @return \yii\web\Response
          * @throws \yii\web\NotFoundHttpException
          */
-        public function actionViewEx($id)
-        {
-            $user = User::findOne(['ff_id' => $id]);
-            if (null == $user) {
-                throw new NotFoundHttpException('User not found!');
-            }
-
-            return $this->redirect(Url::to(['view', 'id' => $user->id]));
-        }
+//        public function actionViewEx($id)
+//        {
+//            $user = User::findOne(['ff_id' => $id]);
+//            if (null == $user) {
+//                throw new NotFoundHttpException('User not found!');
+//            }
+//
+//            return $this->redirect(Url::to(['view', 'id' => $user->id]));
+//        }
     }
